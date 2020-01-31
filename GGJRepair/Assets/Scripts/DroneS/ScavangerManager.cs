174c70645103cea56c063323f11171d51e18c0b5
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2020-01-31 21:28:56 +0000

    Update ScavangerManager.cs

--- GGJRepair/Assets/Scripts/DroneS/ScavangerManager.cs
+++ GGJRepair/Assets/Scripts/DroneS/ScavangerManager.cs
@@ -20,9 +20,9 @@ public class ScavangerManager : DroneManager
         if (dronesOnShip > 0)
         {
             GameObject scavenger = Instantiate(scavengerPrefab);
+            dronesOnShip--;
         }
         
     }
     
-
 }
