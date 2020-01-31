38c0003f47c9cbc745acd07ed079b2c4a7dc6822
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2020-01-31 19:53:17 +0000

    World Tile Properties
    - Added Properties to Each World Tile

--- GGJRepair/Assets/Scripts/WorldTile.cs
+++ GGJRepair/Assets/Scripts/WorldTile.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class WorldTile : MonoBehaviour
 {
 
+    public enum TileTypes
+    {
+        Dessert,
+        Ocean,
+        Forest
+    }
+
+    //Properties for this world tile
+    private bool discovered;
+    [Range(0,100)]
+    private float repairState;
+    [Range(0, 100)]
+    private float remainingResources;
+    private bool beingRepaired;
+
+    [SerializeField]
+    private TileTypes ourType;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //Initalise values
+        discovered = false;
+        repairState = 0f;
+        remainingResources = 0f;
+        beingRepaired = false;
     }
 
     // Update is called once per frame
