3027f13fea3578d421f8731ea667c812229f2467
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2020-01-31 20:37:53 +0000

    Sprite Fading

--- GGJRepair/Assets/Scripts/WorldTile.cs
+++ GGJRepair/Assets/Scripts/WorldTile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class WorldTile : MonoBehaviour
 {
 
@@ -23,6 +24,10 @@ public class WorldTile : MonoBehaviour
     [SerializeField]
     private TileTypes ourType;
 
+    [SerializeField]
+    private Sprite goodSprite, badSprite;
+    private SpriteRenderer spriteRenderer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -32,11 +37,65 @@ public class WorldTile : MonoBehaviour
         repairState = 0f;
         remainingResources = 0f;
         beingRepaired = false;
+
+        //Start with the badSprite
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = badSprite;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            StartCoroutine(FadeToSprite(5, goodSprite));
+        }
+    }
+
+    /// <summary>
+    /// Fade this tile to have a new sprite
+    /// This objects sprite will be changes
+    /// </summary>
+    /// <param name="time"></param>
+    /// <param name="newSprite"></param>
+    /// <returns></returns>
+    IEnumerator FadeToSprite(float time, Sprite newSprite)
+    {
+        /*This Function Creates a new sprite object behind this one,
+         * fades this sprite out, then once fully faded deletes the new
+         * sprite object and sets this object sprite to the new sprite
+         */
+
+
+        //Init values
+        GameObject newSpriteObj; //New obj that will be created to put the new sprite on while we transision
+        SpriteRenderer thisObjSpriteRenderer = gameObject.GetComponent<SpriteRenderer>(); //Sprite Renderer on this obj
+
+        //Create Good Sprite behind this one on a new game obj
+        newSpriteObj = new GameObject();
+        newSpriteObj.transform.position = transform.position + new Vector3(0, 0, 10);
+        newSpriteObj.transform.parent = transform;
+        newSpriteObj.name = "TransisionSprite";
+        //Add a sprite renderer to the new object and assign it the new sprie
+        newSpriteObj.AddComponent<SpriteRenderer>().sprite = newSprite;
+
+        //Fade out Old Sprite
+        float currentAlpha = thisObjSpriteRenderer.color.a;
+        float intendedAlpha = 0.0f;
+        for(float t = 0.0f; t < 1.0f; t += Time.deltaTime / time)
+        {
+            Color newColour = new Color(1, 1, 1, Mathf.Lerp(currentAlpha, intendedAlpha, t));
+            thisObjSpriteRenderer.color = newColour;
+            yield return null;
+           
+        }
+
+        //Change This Objects Sprite and destory the 
+        //old object
+        thisObjSpriteRenderer.sprite = goodSprite;
+        thisObjSpriteRenderer.color = Vector4.one;
+        Destroy(newSpriteObj);
+
     }
 }
