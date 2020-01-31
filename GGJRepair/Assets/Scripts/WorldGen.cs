f8aa62a6f5762ac2427361887d1af47de0441999
Lewis Hammond <55703082+LewisHammond-uog@users.noreply.github.com>
2020-01-31 19:43:57 +0000

    Basic World Generator
    Added Basic World Generator that generates chunks in the world and fills those chunks with tiles

---
+++ GGJRepair/Assets/Scripts/WorldGen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject clusterPrefab;
+
+    [SerializeField]
+    private Vector2 worldDimentions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Generate chunks in the world
+        for(int x = 1; x < worldDimentions.x; x += 2)
+        {
+            for(int y = 1; y < worldDimentions.y; y += 2)
+            {
+                GameObject newCluster = Instantiate(clusterPrefab);
+                newCluster.transform.position = new Vector2(x, y);
+                newCluster.transform.parent = transform;
+                newCluster.name = ($"World Chunk {x},{y}");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
