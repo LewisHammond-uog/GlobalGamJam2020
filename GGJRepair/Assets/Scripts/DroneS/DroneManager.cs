94619f87dac337c5f6bb02e3ebbeafb9ee00bdbc
RhysWareham <55702694+RhysWareham@users.noreply.github.com>
2020-02-02 15:12:55 +0000

    Resource text update completed

--- GGJRepair/Assets/Scripts/DroneS/DroneManager.cs
+++ GGJRepair/Assets/Scripts/DroneS/DroneManager.cs
@@ -7,4 +7,9 @@ public abstract class DroneManager : MonoBehaviour
     public int totalDrones;
     public int donesOnShip;
     public Vector3 baseLocation;
+
+    public void ReturnDrone()
+    {
+        donesOnShip++;
+    }
 }
